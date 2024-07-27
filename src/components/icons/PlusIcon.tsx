fe5fc8d75122767baeaf9d5ecc78ce732d4008d0
yan-batista <ybatistasouza@hotmail.com>
2024-07-27 16:16:41 -0300

    feat: change delay with buttons

--- src/components/icons/PlusIcon.tsx
+++ src/components/icons/PlusIcon.tsx
@@ -1,7 +1,25 @@
-const FolderIcon = () => {
+interface PlusIconProps {
+  action?: () => void
+}
+
+const PlusIcon: React.FC<PlusIconProps> = ({action = () => {}}: PlusIconProps) => {
   return(
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="24" 
+      height="24"
+       viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      className="feather feather-plus"
+      onClick={action}>
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
   )
 }
 
-export default FolderIcon;
+export default PlusIcon;
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import MinusIcon from "../../components/icons/MinusIcon";
 import PlusIcon from "../../components/icons/PlusIcon";
 import Switch from "../../components/Switch";
 import "./style.css"
 
 const SettingsPage = () => {
+  const [delay, setDelay] = useState<number>(10);
+  
+function increaseDelay() {
+  setDelay(prevState => prevState + 1)
+}
+
+function decreaseDelay() {
+  setDelay(prevState => prevState--)
+}
+
+function onChangeDelay(event: React.ChangeEvent<HTMLInputElement>) {
+  setDelay(Number(event.currentTarget.value))
+}
+
   return(
     <div className="settings">
       <div className="setting_container">
@@ -19,9 +34,9 @@ const SettingsPage = () => {
       <div className="setting_container">
         <p>Delay (min)</p>
         <div className="delay_options">
-          <input type="text" defaultValue={10}/>
-          <MinusIcon />
-          <PlusIcon />
+          <input type="text" value={delay} onChange={onChangeDelay}/>
+          <MinusIcon action={decreaseDelay}/>
+          <PlusIcon action={increaseDelay}/>
         </div>
       </div>
 
