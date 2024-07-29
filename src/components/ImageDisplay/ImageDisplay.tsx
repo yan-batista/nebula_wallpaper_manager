ad9c69764bc58bf5d7231121ba2afa1df48874a5
yan-batista <ybatistasouza@hotmail.com>
2024-07-29 03:05:21 -0300

    fix: image display; added scrollbar + changed image height

--- src/components/ImageDisplay/ImageDisplay.tsx
+++ src/components/ImageDisplay/ImageDisplay.tsx
@@ -9,7 +9,7 @@ interface ImageDisplayProps {
 const ImageDisplay: React.FC<ImageDisplayProps> = ({name, image_path = ""}: ImageDisplayProps) => {
   return (
     <div className="image_display">
-      {image_path == "" ? <ImageFileIcon /> : <img src={window.__TAURI__.convertFileSrc(image_path, "asset")} className="thumbnail"/>}
+      {image_path == "" ? <ImageFileIcon /> : <img src={window.__TAURI__.convertFileSrc(image_path, "asset")} className="thumbnail" loading="lazy"/>}
       <p>{name}</p>
     </div>
   )
